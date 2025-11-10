cd72c541e0ea74e8f364ed679b5e71103e2544a9
amuif <amudihayat@gmail.com>
2025-11-10 22:27:11 +0300

    fix:corp issue

--- index.js
+++ index.js
@@ -36,6 +36,20 @@ app.use(
 // Performance middleware
 app.use(compression());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Range');
+  res.setHeader('Access-Control-Expose-Headers', 'Content-Range, Accept-Ranges, Content-Length');
+
+  res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
 // CORS configuration
 app.use(
   cors({
